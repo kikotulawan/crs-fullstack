5ec1eb16e6ad5bb36bad01d134d9d13596409ff4
Ezikiel Tulawan <ezikielpurautlawan@gmail.com>
2024-02-25 22:25:21 +0800

    HOTFIX: Added a break tag to Homepage title

--- frontend/src/views/homepage/Homepage.vue
+++ frontend/src/views/homepage/Homepage.vue
@@ -1,47 +1,31 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-import HomeNavbar from "@components/HomeNavbar.vue";
-import HomeFooter from "@components/HomeFooter.vue";
-import AOD from "@components/AnimatedArrowDown.vue";
+ import { onMounted } from "vue";
+ import HomeNavbar from "@components/HomeNavbar.vue";
+ import HomeFooter from "@components/HomeFooter.vue";
+ import AOD from "@components/AnimatedArrowDown.vue";
 
-onMounted(() => {});
+ onMounted(() => {});
 </script>
 
 <template>
  <HomeNavbar class="animate__animated animate__slideInDown" />
  <main class="min-h-screen h-full container mx-auto pt-32">
-  <section
-   id="hero"
-   class="w-full flex flex-col items-center text-center gap-4 z-10"
-  >
-   <img
-    class="animate__animated animate__fadeInUp"
-    src="@assets/images/San-Miguel-Logo.png"
-    alt="San Miguel Leyte Logo"
-    width="200"
-   />
+  <section id="hero" class="w-full flex flex-col items-center text-center gap-4 z-10">
+   <img class="animate__animated animate__fadeInUp" src="@assets/images/San-Miguel-Logo.png" alt="San Miguel Leyte Logo" width="200" />
    <h1 class="text-4xl lg:text-5xl font-black max-w-4xl animate__animated animate__zoomIn">
     <span class="text-blue-500">EasyCedula</span> - Your Convenient Cedula Request System for
-    <span class="text-blue-500">San Miguel, Leyte</span>
+    <span class="text-blue-500"><br />San Miguel, Leyte</span>
    </h1>
    <h4 class="text-xl font-medium text-gray-400 animate__animated animate__fadeInUp">Obtain Your Cedula Hassle-Free with EasyCedula</h4>
    <h5 class="text-sm font-medium text-black mt-10 animate__animated animate__fadeInUp">Scroll Down</h5>
    <AOD class="animate__animated animate__fadeInUp" />
   </section>
-  <section
-   id="why-easycedula"
-   class="lg:mt-28 p-10 animate__animated animate__fadeInUp"
-  >
-   <h3 class="text-xl font-semibold mb-5 before:content-[''] before:border-t before:border-black before:inline-block before:my-0 before:mx-2 before:-translate-y-4 before:w-10 before:lg:w-16">
-    Why EasyCedula?
-   </h3>
+  <section id="why-easycedula" class="lg:mt-28 p-10 animate__animated animate__fadeInUp">
+   <h3 class="text-xl font-semibold mb-5 before:content-[''] before:border-t before:border-black before:inline-block before:my-0 before:mx-2 before:-translate-y-4 before:w-10 before:lg:w-16"> Why EasyCedula? </h3>
    <div class="w-full flex flex-col lg:flex-row gap-10 items-stretch">
     <div class="bg-white shadow-lg rounded-lg p-5">
      <h1 class="text-xl lg:text-2xl text-center mb-5 font-bold mx-auto text-blue-500">Simple Online Process</h1>
-     <p>
-      Say goodbye to waiting in line for hours. With EasyCedula, you can request your Cedula from the comfort of your home or office. Our user-friendly interface makes the process straightforward and
-      accessible.
-     </p>
+     <p> Say goodbye to waiting in line for hours. With EasyCedula, you can request your Cedula from the comfort of your home or office. Our user-friendly interface makes the process straightforward and accessible. </p>
     </div>
     <div class="bg-white shadow-lg rounded-lg p-5">
      <h1 class="text-xl lg:text-2xl text-center mb-5 font-bold mx-auto text-blue-500">Fast and Efficient</h1>
@@ -57,20 +41,11 @@ onMounted(() => {});
     </div>
    </div>
   </section>
-  <section
-   id="how-it-works"
-   class="lg:mt-20 p-10"
-  >
-   <h3 class="text-xl font-semibold mb-5 before:content-[''] before:border-t before:border-black before:inline-block before:my-0 before:mx-2 before:-translate-y-4 before:w-10 before:lg:w-16">
-    How It Works?
-   </h3>
+  <section id="how-it-works" class="lg:mt-20 p-10">
+   <h3 class="text-xl font-semibold mb-5 before:content-[''] before:border-t before:border-black before:inline-block before:my-0 before:mx-2 before:-translate-y-4 before:w-10 before:lg:w-16"> How It Works? </h3>
    <div class="w-full flex flex-col lg:flex-row gap-10">
     <div class="w-full overflow-hidden">
-     <img
-      class="w-full h-full"
-      src="@assets/images/drawing.png"
-      alt="How it works"
-     />
+     <img class="w-full h-full" src="@assets/images/drawing.png" alt="How it works" />
     </div>
     <div class="w-full max-w-[500px] flex flex-col gap-3">
      <div class="bg-white shadow-lg rounded-lg p-5">
